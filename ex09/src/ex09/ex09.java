44ba4a4b81b83a0064ab2fda19c1c3fc671ad7ed
LucasLnn <lucas.nnascimento@aluno.faculdadeimpacta.com.br>
2025-09-10 20:02:47 -0300

    Add files via upload

---
+++ ex09/src/ex09/ex09.java
@@ -0,0 +1,28 @@
+package ex09;
+
+import java.util.Scanner;
+
+public class ex09 {
+
+    public static void main(String[] args) {
+
+
+        final double COTACAO_DOLAR = 3.45;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Quanto dinheiro você tem na carteira (em R$)? ");
+        double dinheiroReais = scanner.nextDouble();
+
+
+        double dinheiroDolares = dinheiroReais / COTACAO_DOLAR;
+
+
+        System.out.printf("Com R$ %.2f, você pode comprar US$ %.2f.%n", dinheiroReais, dinheiroDolares);
+
+
+        scanner.close();
+    }
+}
